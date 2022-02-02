6c5fc517234a7c6ec7aba96fc70657689c580837
Arissa <rami.34000@gmail.com>
2022-02-02 23:02:54 +0100

    Use a new way to import the native library from a fat jar

--- SpaceMouseWrapper/src/main/java/com/rm/spacemousewrapper/AbsLoader.java
+++ SpaceMouseWrapper/src/main/java/com/rm/spacemousewrapper/AbsLoader.java
@@ -1,13 +1,36 @@
 package com.rm.spacemousewrapper;
 
+import com.badlogic.gdx.jnigen.BuildTarget;
 import com.badlogic.gdx.utils.SharedLibraryLoader;
+
+import java.io.IOException;
 
 public abstract class AbsLoader implements AutoCloseable {
 
     protected int clientId = 0;
 
     static {
-        new SharedLibraryLoader().load("3Dx");
+        try {
+            
+            BuildTarget.TargetOs os;
+            if(SharedLibraryLoader.isMac){
+                os = BuildTarget.TargetOs.MacOsX;
+            }else if(SharedLibraryLoader.isWindows){
+                os = BuildTarget.TargetOs.Windows;
+            }else if(SharedLibraryLoader.isLinux){
+                os = BuildTarget.TargetOs.Linux;
+            }else if(SharedLibraryLoader.isAndroid){
+                os = BuildTarget.TargetOs.Android;
+            }else{
+                os = BuildTarget.TargetOs.IOS;
+            }
+
+            String osFolder = os.toString().toLowerCase() + (SharedLibraryLoader.isARM ? "arm" : "") + (SharedLibraryLoader.is64Bit ? "64" : "32");
+            NativeUtils.loadLibraryFromJar("/libs/"+ osFolder +"/"+new SharedLibraryLoader().mapLibraryName("3Dx"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
